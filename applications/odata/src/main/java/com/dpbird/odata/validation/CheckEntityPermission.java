07d42360aa5645acd08a96c24ef372b2b49a6b31
SunCanyang <try028@163.com>
2023-08-30 10:48:37 +0800

    update err Msg

--- applications/odata/src/main/java/com/dpbird/odata/validation/CheckEntityPermission.java
+++ applications/odata/src/main/java/com/dpbird/odata/validation/CheckEntityPermission.java
@@ -3,6 +3,7 @@ package com.dpbird.odata.validation;
 import com.dpbird.odata.OfbizAppEdmProvider;
 import com.dpbird.odata.OfbizODataException;
 import org.apache.ofbiz.base.util.UtilMisc;
+import org.apache.ofbiz.base.util.UtilProperties;
 import org.apache.ofbiz.base.util.UtilValidate;
 import org.apache.ofbiz.entity.Delegator;
 import org.apache.ofbiz.entity.GenericEntityException;
@@ -11,6 +12,7 @@ import org.apache.ofbiz.entity.util.EntityQuery;
 import org.apache.olingo.commons.api.edm.EdmBindingTarget;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -19,14 +21,14 @@ import java.util.Map;
  */
 public class CheckEntityPermission {
     private static final String MODULE = CheckEntityPermission.class.getName();
-    private static final String ERR_MSG = "No access";
 
     /**
      * 验证权限
      */
     public static void checkPermission(Map<String, Object> odataContext, EdmBindingTarget edmBindingTarget) throws OfbizODataException {
+        String errMsg = UtilProperties.getMessage("OdataUiLabels", "verifyMsg.auth.NO_ACCESS", (Locale) odataContext.get("locale"));
         if (edmBindingTarget == null) {
-            throw new OfbizODataException(ERR_MSG);
+            throw new OfbizODataException(errMsg);
         }
         Delegator delegator = (Delegator) odataContext.get("delegator");
         GenericValue userLogin = (GenericValue) odataContext.get("userLogin");
@@ -42,11 +44,11 @@ public class CheckEntityPermission {
             }
             String partyId = userLogin.getString("partyId");
             if (UtilValidate.isEmpty(partyId)) {
-                throw new OfbizODataException(ERR_MSG);
+                throw new OfbizODataException(errMsg);
             }
             GenericValue partyRole = EntityQuery.use(delegator).from("PartyRole").where("partyId", partyId).queryFirst();
             if (UtilValidate.isEmpty(partyRole)) {
-                throw new OfbizODataException(ERR_MSG);
+                throw new OfbizODataException(errMsg);
             }
             if ("SUPER_ADMIN".equals(partyRole.getString("roleTypeId"))) {
                 return;
@@ -54,7 +56,7 @@ public class CheckEntityPermission {
             List<String> currentUserPermission = EntityQuery.use(delegator).from("RoleTypeSecurityPermission")
                     .where("roleTypeId", partyRole.getString("roleTypeId")).getFieldList("permissionId");
             if (!currentUserPermission.contains(needPermission.getString("permissionId"))) {
-                throw new OfbizODataException(ERR_MSG);
+                throw new OfbizODataException(errMsg);
             }
         } catch (GenericEntityException e) {
             throw new OfbizODataException(e.getMessage());
