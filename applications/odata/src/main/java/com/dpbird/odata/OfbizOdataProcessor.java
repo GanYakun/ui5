88da9b7d8c5917101cc3bcf11781a5c01c20eba6
SunCanyang <try028@163.com>
2023-09-20 11:19:11 +0800

    fix query

--- applications/odata/src/main/java/com/dpbird/odata/OfbizOdataProcessor.java
+++ applications/odata/src/main/java/com/dpbird/odata/OfbizOdataProcessor.java
@@ -57,7 +57,7 @@ import static com.dpbird.odata.OdataExpressionVisitor.AGGREGATE_MAP;
 public class OfbizOdataProcessor {
 
     public static final String module = OfbizOdataProcessor.class.getName();
-    public static final int MAX_ROWS = 10000;
+    public static final int MAX_ROWS = 200;
     public static final int EXTRA_QUERY_MAX_RAW = 1000;
     protected Delegator delegator;
     protected LocalDispatcher dispatcher;
